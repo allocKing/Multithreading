87f6d732949a32d7b9ca5eb1ca9f24251d6c417b
谷统鑫 <18686936786@163.com>
2016-05-28 19:55:23 +0800

    新建Mulit-Thread项目 添加第三方框架、工具

---
+++ Mulit-Thread/Mulit-Thread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mulit-Thread
+//
+//  Created by 谷统鑫 on 16/5/28.
+//  Copyright © 2016年 谷统鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mulit-Thread/Mulit-Thread/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mulit-Thread
+//
+//  Created by 谷统鑫 on 16/5/28.
+//  Copyright © 2016年 谷统鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
